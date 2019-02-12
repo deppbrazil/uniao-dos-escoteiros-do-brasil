8585c384390c1facac9db1fe84b3635a01140aa1
Mir Carvalho <deppbrazil@gmail.com>
2019-02-12 14:55:28 -0200

    add home screen + content

--- test/function.js
+++ test/function.js
@@ -1,7 +1,4 @@
-var $doc = $('html, body');
-$('a').click(function() {
-    $doc.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});
+// Button Home Screen //
+function goContent() {
+    location.href = "content.html"
+}
