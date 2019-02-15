4c1f768b3ab3bcff9e5a9cfa9d5fe5fea11d99b1
Mir Carvalho <deppbrazil@gmail.com>
2019-02-14 23:15:36 -0200

    upgrade index

--- function.js
+++ function.js
@@ -1,4 +1,18 @@
-// Button Home Screen //
+// button home screen //
 function goContent() {
     location.href = "content.html"
+}
+
+// progressBar //
+var progress = new Number();
+var Max = new Number();
+var progress = 0;
+var Max = 100;
+
+function start() {
+    if ((progress + 1) < Max) {
+        progress = progress + 1;
+        document.getElementById("progressBar").value = progress;
+        setTimeout("start();", 200);
+    }
 }
